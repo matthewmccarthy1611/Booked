cd4c4da0169fd58a723a4c2172cecbca4919d8d2
mmccarthy64 <mmccarthy64@hotmail.com>
2021-05-27 14:19:13 -0500

    added style.css, index.js fetch get local books

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    renderBooks;
+    getBooks();
 })
-
-let getBooks = () => {
-    return fetch('https://localhost:3000/books')
+function getBooks(){
+    return fetch('http://localhost:3000/books')
         .then(resp => resp.json())
-        .then(books => {
-            books.map(book => renderBooks(book))
-        })
+        .then(books => books.forEach(book => renderBooks(book)))
 }
 
-let renderBooks = (book) => {
-    
+// getBooks().then(books => books.forEach(book => renderBooks(book)))
+
+function renderBooks(book) {
+    console.log(book)
+    const booksContainer = document.getElementById('books-container')
+    let h2 = document.createElement('h2')
+    h2.innerText = book['title']
+
+    let p = document.createElement('p')
+    p.innerText = book['author']
+
+    let divCard = document.createElement('div')
+    divCard.setAttribute('class', 'card')
+
+    divCard.append(h2, p)
+    booksContainer.append(divCard)
 }
