d20b707cd6acab4abefffba476d53707441ea486
mmccarthy64 <mmccarthy64@hotmail.com>
2021-05-27 10:22:31 -0500

    added book scaffold & seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "Basic Economics", author: "Thomas Sowell", page_count: 600)
+Book.create(title: "Alexander Hamilton", author: "Ron Chernow", page_count: 570)
+Book.create(title: "The New Right", author: "Michael Malice", page_count: 280)
+Book.create(title: "Atomic Habits", author: "James Clear", page_count: 250)
+Book.create(title: "Starting Strength", author: "Mark Rippetoe", page_count: 600)
+Book.create(title: "The Fountainhead", author: "Ayn Rand", page_count: 800)
+Book.create(title: "KJV Bible", author: "God", page_count: 1800)
+Book.create(title: "12 Rules for Life", author: "Jordan Peterson", page_count: 450)
+Book.create(title: "Theory and History", author: "Ludwig von Mises", page_count: 450)
+Book.create(title: "Modern Times", author: "Paul Johnson", page_count: 450)
+Book.create(title: "Man, Economy, and State", author: "Murray N. Rothbard", page_count: 450)
